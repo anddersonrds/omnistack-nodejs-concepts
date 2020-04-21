db1cb52dd18136af877e29cf95785b6f474cd60c
Anderson Rodrigues <anddersonrds@gmail.com>
2020-04-21 02:29:36 -0300

    fix-project

--- omnistack-v2/src/app.js
+++ omnistack-v2/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,7 +11,10 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
 });
 
 app.post("/repositories", (request, response) => {
